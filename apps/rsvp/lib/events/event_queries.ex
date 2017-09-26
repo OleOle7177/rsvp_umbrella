5e8149fe90d9b5482d7123a955916e5db8f06054
“Oleg <ole_@bk.ru>
2017-09-26 22:20:28 +0300

    Changeset basic validations

---
+++ apps/rsvp/lib/events/event_queries.ex
@@ -0,0 +1,26 @@
+defmodule Rsvp.EventQueries do
+  import Ecto.Query
+
+  alias Rsvp.{Repo, Events} # alias for Repo and Events
+
+  def get_all do
+    # Repo - это модуль, отвечающий за коннект к бд.
+    # all - метод, возвращающий все, что вернет query
+    # from events Events - query
+    Repo.all(from Events)
+  end
+
+  def get_all_for_location(location) do
+    query = from e in Events,
+      where: e.location == ^location # ^ передана внутрь аргументом, ^ - взять значение
+
+      Repo.all(query)
+  end
+
+  def get_by_id(id) do
+    # Rsvp.Repo.get!(Rsvp.Events, 10) get! - упадет если не будет звписи
+    Repo.get(Events, id)
+  end
+end
+
+# insert to db: Rsvp.Repo.insert %Rsvp.Events{title: "Codefest", location: "The Office", date: Ecto.DateTime.cast!("2016-11-25 12:00:00")}
--- apps/rsvp/lib/events/events.ex
+++ apps/rsvp/lib/events/events.ex
@@ -1,6 +1,8 @@
 defmodule Rsvp.Events do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "events" do
     field :title, :string
     field :location, :string
@@ -9,4 +11,14 @@ defmodule Rsvp.Events do
 
     timestamps()
   end
+
+  @required_fields ~w(title location date)a #массив атомов
+  @optional_fields ~w(description)a
+
+  def changeset(event, params \\ %{}) do
+    event
+      |> cast(params, @required_fields ++ @optional_fields)
+      |> validate_required(@required_fields)
+  end
+
 end
