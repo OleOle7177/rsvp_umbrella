013cc7761e87f80f8a6c7ce3fb6d3a8184a9d822
“Oleg <ole_@bk.ru>
2017-10-04 23:15:33 +0300

    Add list view

--- apps/rsvp_web/lib/rsvp_web_web/views/event_view.ex
+++ apps/rsvp_web/lib/rsvp_web_web/views/event_view.ex
@@ -1,5 +1,6 @@
 defmodule RsvpWebWeb.EventView do
   use RsvpWebWeb, :view
+# Phoenix.View.render(RsvpWebWeb.EventView, "details.html", event: %{title: "Some title", location: "Some location", date: date})
 
   def format_date(date) do
     {{y, m ,d}, _} = NaiveDateTime.to_erl(date)
